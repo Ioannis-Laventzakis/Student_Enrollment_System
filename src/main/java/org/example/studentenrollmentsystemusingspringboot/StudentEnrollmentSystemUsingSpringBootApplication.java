8fd21cbf0bd7ae6a8fa7d85b775ba46ac7d895f3
Ioannis Laventzakis <glaventzakis@gmail.com>
2024-05-06 13:49:26 +0200

    First commit

---
+++ src/main/java/org/example/studentenrollmentsystemusingspringboot/StudentEnrollmentSystemUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.studentenrollmentsystemusingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEnrollmentSystemUsingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentEnrollmentSystemUsingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/studentenrollmentsystemusingspringboot/StudentEnrollmentSystemUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studentenrollmentsystemusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEnrollmentSystemUsingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
